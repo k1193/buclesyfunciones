8f7f49947801f47d5e509fddc6cda7642e2cb9a2
k1193 <61071131+k1193@users.noreply.github.com>
2020-02-28 19:27:37 -0500

    se codifica ej de arreglo indexado y asociativo

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+ echo(" Clase 4 bucles, arreglos y funciones");
+ //array indexado
+ $arreglo=array("Carlos","Neymar","Dimaria","Jesús");
+ echo("<br>");
+ print_r($arreglo);
+ echo("<br>..................................");
+
+ //array asociativo
+
+ $arreglo=array('usuario1'=>"Carlos",'usuario2'=>"Neymar",'usuario3'=>"Dimaria",'usuario4'=>"Jesús");
+ echo("<br>");
+ print_r($arreglo);
+
+
+ 
+
+
+
+
+
+?>
